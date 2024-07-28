fb2bdbad58767ab22024e73d894eb8bcf52f4639
Hardik-S <hardik04singh@gmail.com>
2024-07-28 21:08:26 +0530

    Added OOPs intro

---
+++ OOPs_intro.java
@@ -0,0 +1,6 @@
+public class OOPs_intro {
+    // OOPs comprises of - 1. Class          2. Object             3. Methods
+    //                     e.g. Employee     e.g. Employee_name    e.g. get_Salary()
+    //                                       --> This is a real 
+    //                                           world entity.
+}
