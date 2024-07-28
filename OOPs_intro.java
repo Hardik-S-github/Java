4fe171d5ae2d3a25309f42793891b0edcaf97570
Hardik-S <hardik04singh@gmail.com>
2024-07-28 22:03:34 +0530

    Modified OOPs intro

--- OOPs_intro.java
+++ OOPs_intro.java
@@ -1,6 +1,11 @@
 public class OOPs_intro {
-    // OOPs comprises of - 1. Class          2. Object             3. Methods
+    // OOPs comprises of - 1.Class          2.Object             3.Methods
     //                     e.g. Employee     e.g. Employee_name    e.g. get_Salary()
     //                                       --> This is a real 
     //                                           world entity.
+
+    // Terminologies - 1.Abstraction: Hiding internal details 
+    //                 2.Encapsulation: Encapsulating different things in one container
+    //                 3.Inheritance: Creating a new entity from an existing one
+    //                 4.Polymorphism: Able to perform different tasks with the same entity
 }
